b48fd1bed708b5ad9d7ab08ef1daeaa4128ec724
lilei <nlp.lilei@gmail.com>
2026-03-12 14:28:19 +0800

    upload scripts for sandbox

--- scripts/single_task_run.sh
+++ scripts/single_task_run.sh
@@ -1,2 +1,3 @@
 # make sure the OPENROUTER_API_KEY is set before running
+# use --sandbox to run in a sandbox mode
 claw-eval run --task tasks/T02_email_triage --config model_configs/claude_sonnet_46.yaml
